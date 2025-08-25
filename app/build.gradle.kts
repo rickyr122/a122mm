b19db2133407f9efce16f6505000f3b37835427c
ricky <kupechenko122@gmail.com>
2025-08-25 19:54:08 +0700

    Creating launcher icon - done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,9 @@ dependencies {
 
     implementation("androidx.compose.material:material-icons-extended:1.6.1")
 
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("com.google.android.material:material:1.12.0")
+
 //    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 //
 //    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
